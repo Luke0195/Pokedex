e31bc7bab1ca9a0b639c709b45e4ef8eb75a84a5
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-02 19:56:01 -0300

    chore: add global-styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
-import React from 'react';
+import React from "react";
+import GlobalStyles from './styles/global'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1> Pokedexx </h1>
+      <GlobalStyles />
+    </>
+  );
+};
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+*{
+  padding:0;
+  margin: 0;
+  outline: 0;
+  box-sizing: border-box;
+  font-family: 'Lato','Courier New', Courier, monospace;
+}
+
+html,body, #root{
+  height:100%;
+}
+
+body{
+  background:#1a1a1d;
+}
+
+#root{
+  width: 100%;
+  max-width: 910px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+body , input , button{
+  font-size: 16px;
+}
+
+button{
+  cursor: pointer;
+}
+
+`;
